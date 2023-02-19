86a9c5a26468102920aedc884ef11426c14461f8
Mohamed Refaat <63193470+moRefaat271@users.noreply.github.com>
2023-02-19 02:58:02 +0200

    Complete Project

---
+++ Documentation/ITI-Exam DB Documentation/data/d10.json.js
@@ -0,0 +1,1 @@
+window.repositoryObject = {"object_id":"d10","name":"ITI-Exam System DB","description":null,"summary":[{"field":"Host","value":"."},{"field":"Database","value":"EXAM"},{"field":"DBMS","value":"SQL Server"}],"modules":[],"dbObjects":[{"count":13,"id":"d10t","name":"Tables"},{"count":57,"id":"d10p","name":"Procedures"}],"show_schema":false};
